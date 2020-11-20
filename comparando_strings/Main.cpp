852deefeeede575ff9b725af4075fef49bfcc1c3
Gabriel Fernandes <gabriel_fsbs2@hotmail.com>
2020-11-20 02:38:30 -0300

    implmentando mudancad na map

--- comparando_strings/Main.cpp
+++ comparando_strings/Main.cpp
@@ -1,18 +1,29 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <map>
 using namespace std;
 
 int main()
 {
-    string nome, nomeAux;
+    string nome, aux;
+    int x = 1;
+
     ifstream arquivo("texto.txt");
-    arquivo >> nome, nomeAux;
-    nome.compare(nomeAux);
-    if (nome.compare(nomeAux) == 0)
+
+    map<string, int> nomes;
+
+    for (int i = 1; i <= 10; i++)
     {
-        cout << "nomes iguais" << endl;
+        arquivo >> nome;
+        nomes.insert(pair<string, int>(nome, x));
     }
 
-    //cout << nome << endl;
+    map<string, int>::iterator itr;
+    cout << "\tNomes\tfrequencia\n";
+    for (itr = nomes.begin(); itr != nomes.end(); ++itr)
+    {
+        cout << '\t' << itr->first
+             << '\t' << itr->second << '\n';
+    }
 }
---
+++ comparando_strings/tempCodeRunnerFile.cpp
@@ -0,0 +1,1 @@
+    arquivo >> nome, nomeAux;
