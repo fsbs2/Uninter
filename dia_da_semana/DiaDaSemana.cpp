25f576bb02baf3b348c529524890a0cdb2806e8b
Gabriel Fernandes <gabriel_fsbs2@hotmail.com>
2020-12-01 00:56:01 -0300

    implementando melhorias enum

--- dia_da_semana/DiaDaSemana.cpp
+++ dia_da_semana/DiaDaSemana.cpp
@@ -21,18 +21,37 @@ public:
     int ano;
     Dias dias;
 
-    DiaDeSemana()
+    DiaDeSemana(int dia, int mes, int ano, Dias dias)
+        : dia(dia), mes(mes), ano(ano), dias(dias)
     {
     }
 
-    info()
+    void info()
     {
+        if (dias == SABADO || dias == DOMINGO)
+        {
+            cout << "APROVEITE O FINAL DE SEMANA" << endl;
+            cout << dias << endl;
+            cout << dia << endl;
+            cout << mes << endl;
+            cout << ano << endl;
+        }
+        else
+        {
+            cout << "HOJE E DIA DE FEIRA, BOM SERVICO" << endl;
+            cout << dias << endl;
+            cout << dia << endl;
+            cout << mes << endl;
+            cout << ano << endl;
+        }
     }
 };
 
 int main()
 {
-    cout << "Hello world";
-
+    DiaDeSemana d1(29, 11, 2020, DOMINGO);
+    d1.info();
+    DiaDeSemana d2(30, 11, 2020, SEGUNDA);
+    d2.info();
     return 0;
 }
