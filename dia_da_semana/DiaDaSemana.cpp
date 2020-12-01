028d36265c9c07f8b6d46cc652905b8e51b51678
Gabriel Fernandes <gabriel_fsbs2@hotmail.com>
2020-11-30 23:58:05 -0300

    exercicio dia da semana

---
+++ dia_da_semana/DiaDaSemana.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+enum Dias
+{
+    SEGUNDA,
+    TERCA,
+    QUARTA,
+    QUINTA,
+    SEXTA,
+    SABADO,
+    DOMINGO
+};
+
+class DiaDeSemana
+{
+public:
+    int dia;
+    int mes;
+    int ano;
+    Dias dias;
+
+    DiaDeSemana()
+    {
+    }
+
+    info()
+    {
+    }
+};
+
+int main()
+{
+    cout << "Hello world";
+
+    return 0;
+}
